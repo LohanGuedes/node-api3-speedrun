771afddbbdedc4d2f6217b7b1d18f114af8466c1
Lguedes <lohan.guedes@nutrix.tech>
2023-07-20 06:06:32 -0300

    Refactoring

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -7,14 +7,12 @@ function logger(req, res, next) {
 
 async function validateUserId(req, res, next) {
   try {
-    const user = await db.getById(req.params.id);
-    if (!user) {
-      res.status(404);
-      res.json({ message: "user not found" });
-      return;
+    if (!(await db.getById(req.params.id))) {
+      return res.status(404).json({ message: "user not found" });
     }
   } catch (error) {
     console.log(error);
+    return res.status(500).json({ message: "Could not check if user exists" });
   }
   next();
 }
@@ -22,9 +20,7 @@ async function validateUserId(req, res, next) {
 function validateUser(req, res, next) {
   const body = req.body;
   if (!body || !body.name) {
-    res.status(400);
-    res.json({ message: "missing required name field" });
-    return;
+    return res.status(400).json({ message: "missing required name field" });
   }
 
   next();
@@ -33,9 +29,7 @@ function validateUser(req, res, next) {
 function validatePost(req, res, next) {
   const body = req.body;
   if (!body || !body.text) {
-    res.status(400);
-    res.json({ message: "missing required text field" });
-    return;
+    return res.status(400).json({ message: "missing required text field" });
   }
   next();
 }
